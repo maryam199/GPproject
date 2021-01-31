38e6f9a50aa784ebccaf2b8e5aa7e6b076b23e5d
maryam199 <54041553+maryam199@users.noreply.github.com>
2021-01-31 16:33:30 +0300

    CHANGE made

--- app/src/main/java/com/example/loginscreen/UserOption.java
+++ app/src/main/java/com/example/loginscreen/UserOption.java
@@ -9,11 +9,20 @@ import android.widget.Button;
 
 public class UserOption extends AppCompatActivity {
     private Button button;
+    private Button user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_option);
         button = (Button) findViewById(R.id.button6);
+        user = (Button) findViewById(R.id.button5);
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInfo();
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +34,12 @@ public class UserOption extends AppCompatActivity {
     public void openDieticianOption(){
         Intent intent = new Intent(this, DieticianOption.class);
         startActivity(intent);
+
+    }
+
+    public void openInfo(){
+        Intent intent2 = new Intent(this, ClientOption.class);
+        startActivity(intent2);
 
     }
 }
