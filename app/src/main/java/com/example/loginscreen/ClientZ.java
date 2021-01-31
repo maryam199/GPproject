6570b1cb75fe1db4d9cb76dbf3bd5aa2b0e09581
maryam199 <54041553+maryam199@users.noreply.github.com>
2021-01-31 20:36:10 +0300

    CHANGE made

--- app/src/main/java/com/example/loginscreen/ClientZ.java
+++ app/src/main/java/com/example/loginscreen/ClientZ.java
@@ -2,13 +2,29 @@ package com.example.loginscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ClientZ extends AppCompatActivity {
-
+    private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_z);
+        image = (ImageView) findViewById(R.id.imageView18);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCompletion();
+            }
+        });
+    }
+
+    public void openCompletion(){
+        Intent intent = new Intent(this, QuizCompletion.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/loginscreen/QuizCompletion.java
+++ app/src/main/java/com/example/loginscreen/QuizCompletion.java
@@ -2,13 +2,29 @@ package com.example.loginscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizCompletion extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_completion);
+        button = (Button) findViewById(R.id.button8);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAttenttion();
+            }
+        });
+    }
+
+    public void openAttenttion(){
+        Intent intent = new Intent(this, Attenttion.class);
+        startActivity(intent);
+
     }
 }
