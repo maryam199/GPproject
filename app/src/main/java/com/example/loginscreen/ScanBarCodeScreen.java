df38ba9beeb5f572a16dcedead83ead6e1719389
sonyc <heboo-1419@hotmail.com>
2021-02-02 20:43:18 +0300

    barcode screen

---
+++ app/src/main/java/com/example/loginscreen/ScanBarCodeScreen.java
@@ -0,0 +1,28 @@
+package com.example.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ScanBarCodeScreen extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button =(Button ) findViewById(R.id.button );
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+}
