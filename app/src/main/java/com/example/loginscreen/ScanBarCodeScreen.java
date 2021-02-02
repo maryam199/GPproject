9d7dd13e57986c7d940cf68852b0f42c9269980e
maryam199 <54041553+maryam199@users.noreply.github.com>
2021-02-02 20:54:31 +0300

    .

--- app/src/main/java/com/example/loginscreen/ScanBarCodeScreen.java
+++ app/src/main/java/com/example/loginscreen/ScanBarCodeScreen.java
@@ -12,7 +12,7 @@ public class ScanBarCodeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scan_bar_code_screen);
         button =(Button ) findViewById(R.id.button );
         button.setOnClickListener(new View.OnClickListener() {
             @Override
