16113890e7ff349357ad14a8d7d1ea373759f930
maryam199 <54041553+maryam199@users.noreply.github.com>
2021-02-10 01:48:51 +0300

    added UserOption code ( added type of the user to firebase - client or dietician - )

---
+++ app/src/main/java/com/example/loginscreen/Client.java
@@ -0,0 +1,5 @@
+package com.example.loginscreen;
+
+public class Client {
+
+}
--- app/src/main/java/com/example/loginscreen/UserOption.java
+++ app/src/main/java/com/example/loginscreen/UserOption.java
@@ -1,45 +1,67 @@
 package com.example.loginscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserOption extends AppCompatActivity {
-    private Button button;
-    private Button user;
+    private Button dietician, client;
+    private DatabaseReference databaseR;
+    private FirebaseDatabase firebaseD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_option);
-        button = (Button) findViewById(R.id.button6);
-        user = (Button) findViewById(R.id.button5);
-        user.setOnClickListener(new View.OnClickListener() {
+        dietician = (Button) findViewById(R.id.button6);
+        client = (Button) findViewById(R.id.button5);
+        firebaseD = FirebaseDatabase.getInstance();
+        databaseR = firebaseD.getInstance().getReference("User").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("type");
+
+        client.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openInfo();
+                String data = "مستخدم";
+                databaseR.setValue(data);
+                openClientOption();
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
+        dietician.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String dat = "أخصائي تغذية";
+                databaseR.setValue(dat);
                 openDieticianOption();
             }
         });
+
+
+    }
+
+    public void openClientOption(){
+        Intent intent = new Intent(this, ClientOption.class);
+        startActivity(intent);
     }
 
     public void openDieticianOption(){
         Intent intent = new Intent(this, DieticianOption.class);
         startActivity(intent);
-
-    }
-
-    public void openInfo(){
-        Intent intent2 = new Intent(this, ClientOption.class);
-        startActivity(intent2);
-
     }
 }
