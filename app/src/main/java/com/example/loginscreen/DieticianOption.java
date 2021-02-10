81fb7c01cff7d7306557df70ccbb6e7594e0caca
maryam199 <54041553+maryam199@users.noreply.github.com>
2021-02-10 18:26:37 +0300

    ضفت الكود لرفع صورة الشهادة لأخصائي التغذية

--- app/src/main/java/com/example/loginscreen/DieticianOption.java
+++ app/src/main/java/com/example/loginscreen/DieticianOption.java
@@ -1,14 +1,83 @@
 package com.example.loginscreen;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class DieticianOption extends AppCompatActivity {
+   private ImageView image;
+   private Uri imageUri;
+   private FirebaseStorage storage;
+   private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dietician_option);
+
+      image = findViewById(R.id.imageView33);
+      storage = FirebaseStorage.getInstance();
+      storageReference = storage.getReference();
+
+      image.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View v) {
+               choosePicture();
+            }
+        });
+    }
+
+   private void choosePicture(){
+       Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            imageUri = data.getData();
+            image.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture(){
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference riversRef = storageReference.child("images/" + randomKey);
+
+        riversRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Snackbar.make(findViewById(android.R.id.content), "تم تحميل الصورة بنجاح", Snackbar.LENGTH_LONG).show();
+            }
+        })
+      .addOnFailureListener(new OnFailureListener() {
+          @Override
+          public void onFailure(@NonNull Exception e) {
+              Toast.makeText(getApplicationContext(), "خطأ", Toast.LENGTH_LONG).show();
+          }
+      });
     }
 }
