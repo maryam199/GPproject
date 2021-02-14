41e4d6314f68beedbbf6644460a163f384a1c433
Heba <heboo-1419@hotmail.com>
2021-02-14 03:13:54 +0300

    profile Screen

--- app/src/main/java/com/example/loginscreen/DisplyRecipScerrn.java
+++ app/src/main/java/com/example/loginscreen/DisplyRecipScerrn.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfileScreen extends AppCompatActivity {
+public class DisplyRecipScerrn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_screen);
-
+        setContentView(R.layout.activity_disply_recip_scerrn);
     }
 }
